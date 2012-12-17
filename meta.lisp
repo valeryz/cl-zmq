1c56f058e174ffd9b67040fb5a9a46e00cf9aedb
Valeriy Zamarayev <valeriy.zamarayev@gmail.com>
2012-12-17 10:42:15 +0200

    Fixed missing EINTR handling.

--- meta.lisp
+++ meta.lisp
@@ -10,13 +10,28 @@
 
 (in-package :zeromq)
 
-(define-condition error-again (error)
-  ((argument :reader error-again :initarg :argument))
+(defparameter *retry-on-eintr* t
+  "Whether cl-zmq should retry automatically on EINTR error. This should
+  typically be the case, however it is possible that the user code wants to
+  handle this error ")
+
+(define-condition error-base (error)
+  ((errno :reader error-errno :initarg :argument))
   (:report (lambda (condition stream)
              (write-string (convert-from-foreign
-                            (%strerror (error-again condition))
+                            (%strerror (error-errno condition))
                             :string)
                            stream))))
+
+;; for compatibility with older code that might use the `error-again' accessor
+(defun error-again (err)
+  (error-errno err))
+
+(define-condition error-again (error-base)
+  ())
+
+(define-condition error-eintr (error-again)
+  ())
 
 (defmacro defcfun* (name-and-options return-type &body args)
   (let* ((c-name (car name-and-options))
@@ -39,16 +54,21 @@
                  `(progn
                     (defcfun ,name ,return-type
                       ,@args*)
-
                     (defun ,l-name (,@names ,@(when opts-init `(&optional ,@opts-init)))
                       ,docstring
-                      (let ((,ret (,n-name ,@names ,@opts)))
-                        (if ,(if (eq return-type :pointer)
-                                 `(zerop (pointer-address ,ret))
-                                 `(not (zerop ,ret)))
-                            (let ((errno (errno)))
-                              (cond
-                                #-windows
-                                ((eq errno isys:ewouldblock) (error 'error-again :argument errno))
-                                (t (error (convert-from-foreign (%strerror errno) :string)))))
-                            ,ret))))))))
+                      (let (,ret)
+                        (tagbody retry
+                           (setf ,ret (,n-name ,@names ,@opts))
+                           (if ,(if (eq return-type :pointer)
+                                    `(zerop (pointer-address ,ret))
+                                    `(not (zerop ,ret)))
+                               (let ((errno (errno)))
+                                 (cond
+                                   #-windows
+                                   ((eq errno isys:ewouldblock) (error 'error-again :argument errno))
+                                   ((eq errno isys:eintr)
+                                    (unless *retry-on-eintr*
+                                      (cerror "Retry" 'error-eintr :argument errno))
+                                    (go retry))
+                                   (t (error (convert-from-foreign (%strerror errno) :string)))))))
+                        ,ret)))))))
